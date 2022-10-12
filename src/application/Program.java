8c07e4a3e55ce3f8746a4ce32c955e2be188b37c
Rodrigo Garcia da Silva <rgs190385@gmail.com>
2022-10-11 23:46:56 -0300

    exercicio concluido usando StrinBuilder

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,12 +15,25 @@ public class Program {
 		Comment c1 = new Comment("Have nice trip!");
 		Comment c2 = new Comment("Wow that`s awesome!");
 		Post p1 = new Post(
-				sdf.parse("21/10/2022    13:42:31"), 
+				sdf.parse("10/10/2022    13:42:31"), 
 				"Traveling to New Zeland",
 				"I`m going to visit this wonderful country ", 12);
 		
 		p1.addComment(c1);
 		p1.addComment(c2);
+		
+		Comment c3 = new Comment("Good night");
+		Comment c4 = new Comment("May the Force be with you");
+		Post p2 = new Post(
+				sdf.parse("08/10/2022    22:42:31"), 
+				"Good night guys",
+				"See you tomorrow", 5);
+		
+		p2.addComment(c3);
+		p2.addComment(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
 
 	}
 }
